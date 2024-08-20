085c260d0ef6025a7a039b47250ecc7d169f15a5
Ralf Siewert <ralf@actyvy.st>
2024-08-20 11:42:55 +0200

    fix initial route

---
+++ app/profile/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import { Link } from "expo-router";
+import { globalStyles } from "../../styles/gobalStyles";
+
+export default function ProfilePage() {
+  return (
+    <>
+      <Text style={globalStyles.heading}>I am the Profilepage</Text>
+    </>
+  );
+}
---
+++ app/search/index.js
@@ -0,0 +1,12 @@
+// app/contact.js
+import { View, Text, Pressable } from "react-native";
+import { Link, router } from "expo-router";
+import { globalStyles } from "../../styles/gobalStyles";
+
+export default function SearchPage() {
+  return (
+    <>
+      <Text style={globalStyles.heading}>I am the search page</Text>
+    </>
+  );
+}
