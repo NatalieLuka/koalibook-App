f775ce047121b1fd63c2708345852f8a68437102
Natalie Lukaschyk <N.Janke@live.de>
2024-09-05 14:34:48 +0200

    reading progress chart

--- components/ProgressChart.js
+++ components/ProgressChart.js
@@ -1,5 +1,16 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, Pressable, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Pressable,
+  StyleSheet,
+  Image,
+} from "react-native";
+import { globalStyles } from "../styles/globalStyles";
+import { COLORS } from "../styles/constants";
+import { Picker } from "@react-native-picker/picker";
+import climbingKoala from "../assets/climbingKoala.png";
 
 const initialData = [
   { day: "Mo", pages: 0 },
@@ -15,6 +26,8 @@ const ProgressChart = () => {
   const [data, setData] = useState(initialData);
   const [currentPage, setCurrentPage] = useState("");
   const [selectedDay, setSelectedDay] = useState("Mo");
+  const maxBarHeight = 150;
+  const maxPages = Math.max(...data.map((entry) => entry.pages), 1);
 
   const updatePages = () => {
     const updatedData = data.map((item) =>
@@ -36,23 +49,54 @@ const ProgressChart = () => {
           value={currentPage}
           onChangeText={(number) => setCurrentPage(number)}
         />
-        <TextInput
-          style={styles.input}
-          placeholder="Day"
-          value={selectedDay}
-          onChangeText={(text) => setSelectedDay(text)}
-        />
-        <Pressable onPress={updatePages} />
+        <Picker
+          selectedValue={selectedDay}
+          style={styles.picker}
+          itemStyle={styles.item}
+          onValueChange={(itemValue) => setSelectedDay(itemValue)}
+        >
+          <Picker.Item label="Monday" value="Mo" />
+          <Picker.Item label="Tuesday" value="Tue" />
+          <Picker.Item label="Wednesday" value="Wed" />
+          <Picker.Item label="Thursday" value="Thu" />
+          <Picker.Item label="Friday" value="Fri" />
+          <Picker.Item label="Saturday" value="Sat" />
+          <Picker.Item label="Sunday" value="Sun" />
+        </Picker>
+
+        <Pressable
+          style={({ pressed }) => [
+            globalStyles.button,
+            {
+              backgroundColor: pressed ? COLORS.primary : COLORS.secondary,
+            },
+          ]}
+          onPress={updatePages}
+        >
+          <Text style={globalStyles.buttonText}>Update</Text>
+        </Pressable>
       </View>
 
       <View style={styles.chartContainer}>
-        {data.map((entry, index) => (
-          <View key={index} style={styles.barContainer}>
-            <Text style={styles.pageText}>{entry.pages}</Text>
-            <View style={[styles.bar, { height: entry.pages * 2 }]} />
-            <Text style={styles.dayText}>{entry.day}</Text>
-          </View>
-        ))}
+        {data.map((entry, index) => {
+          const barHeight =
+            maxPages > 0 ? (entry.pages / maxPages) * maxBarHeight : 0;
+
+          return (
+            <View key={index} style={styles.barContainer}>
+              <Text style={styles.pageText}>{entry.pages}</Text>
+              <View style={[styles.bar, { height: barHeight }]}>
+                {barHeight > 0 && (
+                  <Image
+                    source={climbingKoala}
+                    style={[styles.icon, { bottom: barHeight - 20 }]}
+                  />
+                )}
+              </View>
+              <Text style={styles.dayText}>{entry.day}</Text>
+            </View>
+          );
+        })}
       </View>
     </View>
   );
@@ -72,18 +116,30 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 40,
-    borderColor: "gray",
+    borderColor: COLORS.paragraphLight,
     borderWidth: 1,
     marginRight: 10,
     paddingLeft: 8,
     width: 100,
+    color: COLORS.paragraphLight,
   },
+
+  picker: {
+    width: "50%",
+    marginRight: 10,
+  },
+  item: {
+    height: 50,
+    fontSize: 16,
+    color: COLORS.paragraphLight,
+  },
+
   chartContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
-    width: "100%",
+    width: "120%",
     alignItems: "flex-end",
-    height: 200, // Feste Höhe für das Chart
+    height: 200,
     marginTop: 20,
   },
   barContainer: {
@@ -93,16 +149,16 @@ const styles = StyleSheet.create({
   },
   bar: {
     width: 30,
-    backgroundColor: "#8884d8",
+    backgroundColor: COLORS.primary,
     marginBottom: 5,
+    position: "relative",
   },
-  pageText: {
-    marginBottom: 5,
-    fontSize: 16,
-  },
-  dayText: {
-    marginTop: 5,
-    fontSize: 16,
+  icon: {
+    width: 30,
+    height: 40,
+    position: "absolute",
+    left: "50%",
+    transform: [{ translateY: 20 }, { translateX: 10 }],
   },
 });
 export default ProgressChart;
